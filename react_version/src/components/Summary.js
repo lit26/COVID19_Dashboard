5aa803b37a50d9f19e9697d597fcad28489a0679
Tianning Li <ltianningli@gmail.com>
2020-12-21 17:09:44 -0800

    update credit

--- react_version/src/components/Summary.js
+++ react_version/src/components/Summary.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Summary({currentDate, currentConfirmed, currentDeath}) {
     return (
         <>
-            <p>Data Source: Johns Hopkins CSSE (Subjected to data structure change).</p>
+            <p>Data Source: Johns Hopkins CSSE and HealthData.gov. </p>
             <p>By {currentDate}, there are {currentConfirmed.toLocaleString()} confirmed and {currentDeath.toLocaleString()} deaths cases.</p>
         </>
     )
